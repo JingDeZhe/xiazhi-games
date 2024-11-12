0738dc95347aa253a7e388a3b429b0d9a57cb90f
lotusloli <nymphaeal@qq.com>
2024-11-12 21:15:44 +0800

    chore: 升级依赖

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,13 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: { port: 5178 },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+      },
+    },
+  },
   plugins: [
     vue(),
     AutoImport({
